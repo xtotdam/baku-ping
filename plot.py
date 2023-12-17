46708668c5e47d18aa359424403894fff11fbe00
xtotdam <xtotdam@yandex.ru>
2023-12-17 13:49:19 +0300

    dropped pandas dependency

--- plot.py
+++ plot.py
@@ -1,22 +1,30 @@
 import plotly.graph_objects as go
-import pandas as pd
 
-df = pd.read_csv('ping.log.csv', header=0)
-print(len(df), 'records')
+import csv
+import datetime
 
-df['ne'] = -1
-df['oe'] = -2
+def read_csv(filename):
+    df = dict(date=list(), ping=list(), error=list())
+    with open(filename, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            df['date'].append(datetime.datetime.fromisoformat(row['date']))
+            df['ping'].append(float(row['ping']))
+            df['error'].append(row['error'])
+
+    print(f'{filename}: {len(df["date"])} records')
+    return df
+
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df['date'], y=df['ne'], name='Network error', line_color='red'))
-fig.add_trace(go.Scatter(x=df['date'], y=df['oe'], name='OS error', line_color='black'))
+fig.add_hline(y=-1, line_color='red', label=dict(
+    text="Network error  ", font=dict(color='red'), textposition="end", yanchor="top"))
+fig.add_hline(y=-2, line_color='black', label=dict(
+    text="OS error  ", font=dict(color='black'), textposition="end", yanchor="top"))
 
-fig.add_trace(go.Scatter(x=df['date'], y=df['ping'], text=df['error'], name='Ping data K1', line_color='blue'))
 
-# df = pd.read_csv('ping-5s.log.csv', header=0)
-# fig.add_trace(go.Scatter(x=df['date'], y=df['ping'], text=df['error'], name='Ping data K1 (5s)', line_color='green'))
+df = read_csv('ping.log.csv')
+fig.add_trace(go.Scatter(x=df['date'], y=df['ping'], text=df['error'], name='Ping data', line_color='blue'))
 
-df = pd.read_csv('ping-k3.log.csv', header=0)
-fig.add_trace(go.Scatter(x=df['date'], y=df['ping'], text=df['error'], name='Ping data K3', line_color='magenta'))
-
+fig.update_layout(yaxis_title="Ping time, s")
 fig.show()
